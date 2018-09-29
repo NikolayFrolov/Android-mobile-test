b078a07b058081a5033c006f3f2cd5e8fa12984c
NICK-ASUS\Nick <enotiums@gmail.com>
2018-09-29 18:23:41 +0300

    email login

---
+++ Scripts/Bookmate/Email login/Script1538233971574.groovy
@@ -0,0 +1,37 @@
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
+import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+import com.kms.katalon.core.model.FailureHandling as FailureHandling
+import com.kms.katalon.core.testcase.TestCase as TestCase
+import com.kms.katalon.core.testdata.TestData as TestData
+import com.kms.katalon.core.testobject.TestObject as TestObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import internal.GlobalVariable as GlobalVariable
+
+Mobile.startApplication('C:\\Users\\Nick\\Downloads\\application-dev-debug.apk', true)
+
+Mobile.tap(findTestObject('android.widget.FrameLayout3'), 0)
+
+Mobile.tap(findTestObject('android.widget.FrameLayout3'), 0)
+
+Mobile.tap(findTestObject('android.widget.FrameLayout3'), 0)
+
+Mobile.tap(findTestObject('android.widget.FrameLayout3'), 0)
+
+Mobile.tap(findTestObject('android.widget.TextView5 - '), 0)
+
+Mobile.setText(findTestObject('android.widget.EditText0 -  '), 'pie', 0)
+
+Mobile.tap(findTestObject('android.widget.EditText1'), 0)
+
+Mobile.setText(findTestObject('android.widget.EditText1 (1)'), '1234567', 0)
+
+Mobile.tap(findTestObject('android.widget.FrameLayout5'), 0)
+
+Mobile.closeApplication()
+
